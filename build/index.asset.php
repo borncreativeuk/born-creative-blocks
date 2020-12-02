c526323a8ab74db58efb361e28c09e0df3586f5d
localhost8080 <localhost8080@gmail.com>
2020-12-02 17:15:51 +0000

    test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'a69d1318da753f2e86eab943506d6cdd');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '366fa7f68e5cbd76ad039510d2facaa7');
