32f4882135a713d8a5aa30460d9830846445cfb1
localhost8080 <localhost8080@gmail.com>
2020-12-02 16:59:10 +0000

    test

--- borncreative-blocks.php
+++ borncreative-blocks.php
@@ -13,59 +13,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
+ 
+wp_register_script(
+    'borncreative-block',
+    plugins_url( 'build/index.js', __FILE__ ),
+    $asset_file['dependencies'],
+    $asset_file['version']
+);
+
 
 function borncreative_block_editor_assets() {
-    
-    $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
- 
-    wp_register_script(
-        'borncreative-block',
-        plugins_url( 'build/index.js', __FILE__ ),
-        $asset_file['dependencies'],
-        $asset_file['version']
-    );
-
-    // Enqueue block editor styles
-    wp_register_style(
-        'borncreative-block/stylesheets',
-        plugins_url( 'build/index.css', __FILE__ ),
-        [ 'wp-edit-blocks' ],
-        filemtime( plugin_dir_path( __FILE__ ) . 'build/index.css' ) 
-    );
-
-    $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
-
     wp_enqueue_script(
         'borncreative-block',
         plugins_url( 'build/index.js', __FILE__ ),
         $asset_file['dependencies'],
         $asset_file['version']
     );
-
 };
 
 // and then, we actually have the function run when the editor loads...
-add_action( 'init', 'borncreative_block_editor_assets' );
+add_action( 'enqueue_block_editor_assets', 'borncreative_block_editor_assets' );
 
 
-
-
-/**
- * Enqueue view scripts
- */
-function borncreative_block_view_assets() {
-    if ( is_admin() ) {
-        return;
-    }
-
-    $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
-
-    wp_enqueue_script(
-        'borncreative-block',
-        plugins_url( 'build/index.js', __FILE__ ),
-        $asset_file['dependencies'],
-        $asset_file['version']
-    );
-}
-
-add_action( 'enqueue_block_assets', 'borncreative_block_view_assets' );
+// Enqueue block editor styles
+wp_register_style(
+    'borncreative-block/stylesheets',
+    plugins_url( 'build/index.css', __FILE__ ),
+    [ 'wp-edit-blocks' ],
+    filemtime( plugin_dir_path( __FILE__ ) . 'build/index.css' ) 
+);
