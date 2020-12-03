aeb9b09acda3154dadae36f97d7e8475b8143412
localhost8080 <localhost8080@gmail.com>
2020-12-03 12:58:10 +0000

    zoom effect on images

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '467e9ccb5df5b986127ed48aff34562a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '9addbefab80c67535082a3048a74d6df');
