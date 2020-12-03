993f278a580dc5272c66c465c10b7f62afe314dc
localhost8080 <localhost8080@gmail.com>
2020-12-03 17:35:19 +0000

    curser for images

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '36e6881f4ba4694d4d4cfef68bddc45a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '8654d7608f64a2a81e4832defcf6ff29');
