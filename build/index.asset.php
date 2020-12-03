8f2e5bc3915d754e89e7f1bea967c27c5941db67
localhost8080 <localhost8080@gmail.com>
2020-12-03 15:42:19 +0000

    small css fixes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '077c53671f9fc76e8ae1ae55d9004666');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '5ffc1f10dd6a7678a1872f8d0e7967c5');
