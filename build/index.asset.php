9f0bfb6663d6cdada3fd74a07c71b77dc64abaf2
localhost8080 <localhost8080@gmail.com>
2020-12-04 10:18:59 +0000

    new class for whitespace

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'c39906778d6e8f3a38fa1415503bc2c9');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '39a4fb1a7047f5c27a4fc72754555351');
