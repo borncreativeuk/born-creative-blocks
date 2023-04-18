1decfab1a8faca2c23c0bc05dea87d8892383539
Born Creative <hi@born-creative.co.uk>
2023-04-18 16:36:34 +0100

    initial setup for blocks

--- borncreative-blocks.php
+++ borncreative-blocks.php
@@ -1,14 +1,29 @@
 <?php
-/**
- * Plugin Name: borncreative-blocks
- * Description: Blocks!
- * Version: 0.0.1
- * Plugin URI: https://www.born-creative.co.uk
- * Author: BORN CREATIVE
- * Author URI: https://www.born-creative.co.uk
- *
- * @package borncreative-blocks
- */
+ /*
+  Plugin name: Born Creative Blocks
+  Plugin URI: https://github.com/borncreativeuk/born-creative-blocks
+  Description: plugin with various blocks
+  Author: Born Creative
+  Author URI: https://www.born-creative.co.uk/
+  Version: 0.1
+  License: GPL v3 or later
+  License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+  @package borncreative-blocks
+
+  Born Creative Blocks is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+ 
+  Born Creative Blocks is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with Born Creative Blocks. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+  */
 
 
 // Exit if accessed directly.
