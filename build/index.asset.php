e44a99c823d3a4b7865e1c02374d6fd0ff3b5745
Born Creative <hi@born-creative.co.uk>
2023-04-18 17:20:08 +0100

    removing colours so it defaults to theme

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '39f1ffdb6c288833b77a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '6fbcced1dc0fa976df3f');
