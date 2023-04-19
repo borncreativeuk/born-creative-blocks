995f684206e2be783bca12eec5400e47f8825c3d
Born Creative <hi@born-creative.co.uk>
2023-04-19 13:22:36 +0100

    fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '951c9a7046c47a4bd3c8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'ba99568df09ca1e32995');
