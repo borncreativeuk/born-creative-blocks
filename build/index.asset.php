113ea8d982c80f8a7a7b5729678bdaaffb838619
Born Creative <hi@born-creative.co.uk>
2023-04-19 15:56:11 +0100

    text-only-block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '6eb3c13ab70fbd194a7d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '0eacd2d02836a14eeb02');
--- src/text-only-block/text-only-block.js
+++ src/text-only-block/text-only-block.js
@@ -4,75 +4,157 @@ import './style.view.scss';
 import './style.editor.scss';
 
 import {
-    RichText,
-	InspectorControls
+	RichText
 } from '@wordpress/block-editor';
 
 const { getBlockDefaultClassName } = wp.blocks;
+const { Fragment } = wp.element;
+const { InspectorControls, PanelColorSettings, withColors, getColorClassName } = wp.blockEditor;
 
 
-registerBlockType('borncreative/text-only-block', {
-    title: 'Text Only Block',
-    icon: 'heading',
-    category: 'born-creative-blocks',
+edit_header_block = (props) => {
+	const { className, setAttributes } = props;
+	const { attributes } = props;
+	const { textColor, setTextColor } = props;
+	const { backgroundColor, setBackgroundColor } = props;
 
-    attributes: {
-        heading: {
-            source: 'html',
-            selector: '.copy-bd',
-        },
-    },
+	let custom_color_class;
+	let custom_color_style = {};
+	if (textColor != undefined) {
+		if (textColor.class != undefined) {
+			custom_color_class = textColor.class;
+		} else {
+			custom_color_style.color = textColor.color;
+		}
+	}
 
-    edit(props) {
-        const { className, setAttributes } = props;
-        const { attributes } = props;
+	let custom_backgroundcolor_class;
+	let custom_backgroundcolor_style = {};
+	if (backgroundColor != undefined) {
+		if (backgroundColor.class != undefined) {
+			custom_backgroundcolor_class = backgroundColor.class;
+		} else {
+			custom_backgroundcolor_style.backgroundColor = backgroundColor.color;
+		}
+	}
+	function changeHeading(heading) {
+		// using some nice js features instead of typing
+		// { heading: heading }
+		setAttributes({ heading });
+	}
 
-        function changeHeading(heading) {
-            // using some nice js features instead of typing
-            // { heading: heading }
-            setAttributes({ heading });
-        }
+	return [
+		<Fragment>
+			<InspectorControls>
+				{/* Later, when we have customizable options we will add stuff here! */}
+				<div
+					style={{
+						padding: '1em 0',
+					}}
+				>
+					Options
+				</div>
+				<PanelColorSettings
+					title='Color settings'
+					colorSettings={[
+						{
+							value: textColor.color,
+							onChange: setTextColor,
+							label: 'Text color'
+						},
+						{
+							value: backgroundColor.color,
+							onChange: setBackgroundColor,
+							label: 'Background color'
+						},
+					]}
+				/>
+			</InspectorControls>
+			<div className={className + " " + custom_backgroundcolor_class} style={custom_backgroundcolor_style}>
+				<div class="col-full">
+					<RichText
+						className={"copy-bd " + custom_color_class}
+						style={custom_color_style}
+						tagName="div"
+						placeholder="Enter your heading"
+						value={attributes.heading}
+						onChange={changeHeading}
+					/>
+				</div>
+			</div>
+		</Fragment>
+	];
+},
 
-        return [
-            <InspectorControls>
-                {/* Later, when we have customizable options we will add stuff here! */}
-                <div
-                    style={{
-                        padding: '1em 0',
-                    }}
-                >
-                    Options
-                </div>
-            </InspectorControls>,
-            <div className={className}>
-                <div class="col-full">
-                    <RichText
-                        className="copy-bd"
-                        tagName="div"
-                        placeholder="Enter your heading"
-                        value={attributes.heading}
-                        onChange={changeHeading}
-                    />
-                </div>
-            </div>,
-        ];
-    },
+	save_header_block = (props) => {
+		const className = getBlockDefaultClassName('borncreative/text-only-block');
+		const { attributes } = props;
+		const { textColor, customTextColor } = props.attributes;
+		const { backgroundColor, customBackgroundColor } = props.attributes;
 
-    save(props) {
-        const className = getBlockDefaultClassName('borncreative/text-only-block');
-        const { attributes } = props;
+		let custom_color_class;
+		let custom_color_style = {};
+		if (textColor != undefined) {
+			custom_color_class = getColorClassName('color', textColor);
+		}
+		if (customTextColor != undefined) {
+			custom_color_style.color = customTextColor;
+		}
 
-        return (
-            <div className={className} data-aos="fade-up" data-aos-duration="2000">
-                <div class="col-full">
-                    <RichText.Content
-                        class="copy-bd"
-                        tagName="div"
-                        value={attributes.heading}
-                    />
-                </div>
-            </div>
-        );
-    },
-});
+		let custom_backgroundcolor_class;
+		let custom_backgroundcolor_style = {};
+		if (backgroundColor != undefined) {
+			custom_backgroundcolor_class = getColorClassName('background-color', backgroundColor);
+		}
+		if (customBackgroundColor != undefined) {
+			custom_backgroundcolor_style.backgroundColor = customBackgroundColor;
+		}
+		return (
+			<div className={className + " " + custom_backgroundcolor_class} style={custom_backgroundcolor_style} data-aos="fade-up" data-aos-duration="2000">
+				<div class="col-full">
+					<RichText.Content
+						className={"copy-bd " + custom_color_class}
+						style={custom_color_style}
+						tagName="div"
+						value={attributes.heading}
+					/>
+				</div>
+			</div>
+		);
+	},
+
+	registerBlockType('borncreative/text-only-block', {
+		title: 'Text Only Block',
+		icon: 'heading',
+		category: 'born-creative-blocks',
+
+		attributes: {
+			heading: {
+				source: 'html',
+				selector: '.copy-bd',
+			},
+			textColor: {
+				type: 'string'
+			},
+			customTextColor: {
+				type: 'string'
+			},
+			backgroundColor: {
+				type: 'string'
+			},
+			customBackgroundColor: {
+				type: 'string'
+			},
+		},
+		supports: {
+			// Declare support for block's alignment.
+			// This adds support for all the options:
+			// left, center, right, wide, and full.
+			align: true
+		},
+
+		edit: withColors({ textColor: 'color', backgroundColor: 'background-color' })(edit_header_block),
+		save: save_header_block
+
+	});
 
