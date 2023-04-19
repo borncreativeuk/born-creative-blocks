449105df60b84e45c9493697e9aeb3b9925ba73a
Born Creative <hi@born-creative.co.uk>
2023-04-19 15:07:52 +0100

    double title text block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '5a3e81dc92622ad8defd');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '0f8dfdfca661fc97c6b7');
--- src/double-title-text-block/double-title-text-block.js
+++ src/double-title-text-block/double-title-text-block.js
@@ -4,132 +4,186 @@ import './style.view.scss';
 import './style.editor.scss';
 
 import {
-    RichText,
-	InspectorControls
+	RichText,
+	MediaUpload
 } from '@wordpress/block-editor';
 
 const { getBlockDefaultClassName } = wp.blocks;
+const { Fragment } = wp.element;
+const { InspectorControls, PanelColorSettings, withColors, getColorClassName } = wp.blockEditor;
 
+const edit_header_block = (props) => {
+	const { className, setAttributes } = props;
+	const { attributes } = props;
+	const { textColor, setTextColor } = props;
+
+	let custom_color_class;
+	let custom_color_style = {};
+	if (textColor != undefined) {
+		if (textColor.class != undefined) {
+			custom_color_class = textColor.class;
+		} else {
+			custom_color_style.color = textColor.color;
+		}
+	}
+	function changeHeadinga(headinga) {
+		setAttributes({ headinga });
+	}
+	function changeHeadingb(headingb) {
+		setAttributes({ headingb });
+	}
+	function changeContenta(contenta) {
+		setAttributes({ contenta });
+	}
+	function changeContentb(contentb) {
+		setAttributes({ contentb });
+	}
+
+	return [
+		<Fragment>
+			<InspectorControls>
+				{/* Later, when we have customizable options we will add stuff here! */}
+				<div
+					style={{
+						padding: '1em 0',
+					}}
+				>
+					Options
+				</div>
+				<PanelColorSettings
+					title='Color settings'
+					colorSettings={[
+						{
+							value: textColor.color,
+							onChange: setTextColor,
+							label: 'Text color'
+						},
+					]}
+				/>
+			</InspectorControls>
+			<div className={className}>
+				<div class="col-full">
+					<RichText
+						className={"headinga " + custom_color_class}
+						style={custom_color_style}
+						tagName="h2"
+						placeholder="Enter your heading"
+						value={attributes.headinga}
+						onChange={changeHeadinga}
+					/>
+					<RichText
+						className={"contenta " + custom_color_class}
+						style={custom_color_style}
+						tagName="p"
+						placeholder="Enter your content"
+						value={attributes.contenta}
+						onChange={changeContenta}
+					/>
+					<hr />
+					<RichText
+						className={"headingb " + custom_color_class}
+						style={custom_color_style}
+						tagName="h2"
+						placeholder="Enter your heading"
+						value={attributes.headingb}
+						onChange={changeHeadingb}
+					/>
+					<RichText
+						className={"contentb " + custom_color_class}
+						style={custom_color_style}
+						tagName="p"
+						placeholder="Enter your content"
+						value={attributes.contentb}
+						onChange={changeContentb}
+					/>
+				</div>
+			</div>
+		</Fragment>
+	];
+}
+const save_header_block = (props) => {
+	const className = getBlockDefaultClassName('borncreative/double-title-text-block');
+	const { attributes } = props;
+	const { textColor, customTextColor } = props.attributes;
+
+	let custom_color_class;
+	let custom_color_style = {};
+	if (textColor != undefined) {
+		custom_color_class = getColorClassName('color', textColor);
+	}
+	if (customTextColor != undefined) {
+		custom_color_style.color = customTextColor;
+	}
+	return (
+		<div className={className} data-aos="fade-up" data-aos-duration="2000">
+			<div class="col-full">
+				<RichText.Content
+					className={"headinga " + custom_color_class}
+					style={custom_color_style}
+					tagName="h2"
+					value={attributes.headinga}
+				/>
+				<RichText.Content
+					className={"contenta " + custom_color_class}
+					style={custom_color_style}
+					tagName="p"
+					value={attributes.contenta}
+				/>
+				<hr />
+				<RichText.Content
+					className={"headingb " + custom_color_class}
+					style={custom_color_style}
+					tagName="h2"
+					value={attributes.headingb}
+				/>
+				<RichText.Content
+					className={"contentb " + custom_color_class}
+					style={custom_color_style}
+					tagName="p"
+					value={attributes.contentb}
+				/>
+			</div>
+		</div>
+	);
+}
 
 registerBlockType('borncreative/double-title-text-block', {
-    title: 'Double Title and Text Block',
-    icon: 'layout',
-    category: 'born-creative-blocks',
+	title: 'Double Title and Text Block',
+	icon: 'layout',
+	category: 'born-creative-blocks',
 
-    attributes: {
-        headinga: {
-            source: 'html',
-            selector: '.headinga',
-        },
-        headingb: {
-            source: 'html',
-            selector: '.headingb',
-        },
-        contenta: {
-            source: 'html',
-            selector: '.contenta',
-        },
-        contentb: {
-            source: 'html',
-            selector: '.contentb',
-        },
-    },
+	attributes: {
+		headinga: {
+			source: 'html',
+			selector: '.headinga',
+		},
+		headingb: {
+			source: 'html',
+			selector: '.headingb',
+		},
+		contenta: {
+			source: 'html',
+			selector: '.contenta',
+		},
+		contentb: {
+			source: 'html',
+			selector: '.contentb',
+		},
+		textColor: {
+			type: 'string'
+		},
+		customTextColor: {
+			type: 'string'
+		},
+	},
+	supports: {
+		// Declare support for block's alignment.
+		// This adds support for all the options:
+		// left, center, right, wide, and full.
+		align: true
+	},
 
-    edit(props) {
-        const { className, setAttributes } = props;
-        const { attributes } = props;
-
-        function changeHeadinga(headinga) {
-            setAttributes({ headinga });
-        }
-        function changeHeadingb(headingb) {
-            setAttributes({ headingb });
-        }
-        function changeContenta(contenta) {
-            setAttributes({ contenta });
-        }
-        function changeContentb(contentb) {
-            setAttributes({ contentb });
-        }
-
-        return [
-            <InspectorControls>
-                {/* Later, when we have customizable options we will add stuff here! */}
-                <div
-                    style={{
-                        padding: '1em 0',
-                    }}
-                >
-                    Options
-                </div>
-            </InspectorControls>,
-            <div className={className}>
-                <div class="col-full">
-                    <RichText
-                        className="headinga"
-                        tagName="h2"
-                        placeholder="Enter your heading"
-                        value={attributes.headinga}
-                        onChange={changeHeadinga}
-                    />
-                    <RichText
-                        className="contenta"
-                        tagName="p"
-                        placeholder="Enter your content"
-                        value={attributes.contenta}
-                        onChange={changeContenta}
-                    />
-                    <hr />
-                    <RichText
-                        className="headingb"
-                        tagName="h2"
-                        placeholder="Enter your heading"
-                        value={attributes.headingb}
-                        onChange={changeHeadingb}
-                    />
-                    <RichText
-                        className="contentb"
-                        tagName="p"
-                        placeholder="Enter your content"
-                        value={attributes.contentb}
-                        onChange={changeContentb}
-                    />
-                </div>
-            </div>,
-        ];
-    },
-
-    save(props) {
-        const className = getBlockDefaultClassName('borncreative/double-title-text-block');
-        const { attributes } = props;
-
-        return (
-            <div className={className} data-aos="fade-up" data-aos-duration="2000">
-                <div class="col-full">
-                    <RichText.Content
-                        class="headinga"
-                        tagName="h2"
-                        value={attributes.headinga}
-                    />
-                    <RichText.Content
-                        class="contenta"
-                        tagName="p"
-                        value={attributes.contenta}
-                    />
-                    <hr />
-                    <RichText.Content
-                        class="headingb"
-                        tagName="h2"
-                        value={attributes.headingb}
-                    />
-                    <RichText.Content
-                        class="contentb"
-                        tagName="p"
-                        value={attributes.contentb}
-                    />
-                </div>
-            </div>
-        );
-    },
+	edit: withColors({ textColor: 'color' })(edit_header_block),
+	save: save_header_block
 });
 
