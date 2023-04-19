08039bc4ce1731526fe9c94f9bb922a45c19311f
Born Creative <hi@born-creative.co.uk>
2023-04-19 10:58:29 +0100

    header and video header block colour pickers

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '6fbcced1dc0fa976df3f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '20a7da8535a60b6da560');
---
+++ src/header-block-video/header-block-video.js
@@ -0,0 +1,183 @@
+import { registerBlockType } from '@wordpress/blocks';
+
+import './style.view.scss';
+import './style.editor.scss';
+
+import {
+	RichText,
+	MediaUpload
+} from '@wordpress/block-editor';
+
+const { getBlockDefaultClassName } = wp.blocks;
+const { Fragment } = wp.element;
+const { InspectorControls, PanelColorSettings, withColors, getColorClassName } = wp.blockEditor;
+
+
+const edit_header_block = (props) => {
+	const { className, setAttributes } = props;
+	const { attributes } = props;
+	const { textColor, setTextColor } = props;
+
+	let custom_color_class;
+	let custom_color_style = {};
+	if (textColor != undefined) {
+		if (textColor.class != undefined) {
+			custom_color_class = textColor.class;
+		} else {
+			custom_color_style.color = textColor.color;
+		}
+	}
+
+	// we create a function that will take the changes from RichText
+	// and update the attributes
+	function changeBodyContent(changes) {
+		setAttributes({
+			bodyContent: changes
+		})
+	}
+
+	function changeHeading(heading) {
+		// using some nice js features instead of typing
+		// { heading: heading }
+		setAttributes({ heading });
+	}
+
+	function selectImage(value) {
+		console.log(value);
+		setAttributes({
+			image: value.url,
+		})
+	}
+
+	return [
+		<Fragment>
+			<InspectorControls>
+				{/* Later, when we have customizable options we will add stuff here! */}
+				<div
+					style={{
+						padding: '1em 0',
+					}}
+				>
+					Options
+				</div>
+				<PanelColorSettings
+					title='Color settings'
+					colorSettings={[
+						{
+							value: textColor.color,
+							onChange: setTextColor,
+							label: 'Text color'
+						},
+					]}
+				/>
+			</InspectorControls>,
+			<div className={className}>
+				<div className="media">
+					<MediaUpload
+						onSelect={selectImage}
+						render={({ open }) => {
+							return (
+								<video onClick={open} autoplay="autoplay" muted>
+									<source src={attributes.image} type="video/mp4" />
+								</video>
+							);
+						}}
+					/>
+				</div>
+				<div className="copy">
+					<div class="col-full">
+						<RichText
+							className={"copy-hd " + custom_color_class}
+							style={custom_color_style}
+							tagName="h2"
+							placeholder="Enter your heading"
+							value={attributes.heading}
+							onChange={changeHeading}
+						/>
+						{/* Content is replaced by this guy.
+				We determin the class name and the html tag that
+				we want it to show as. */}
+						<RichText
+							className={"copy-bd " + custom_color_class}
+							style={custom_color_style}
+							tagName="h3"
+							placeholder="Enter your text here"
+							value={attributes.bodyContent}
+							onChange={changeBodyContent}
+						/>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	];
+}
+const save_header_block = (props) => {
+	const className = getBlockDefaultClassName('borncreative/header-block');
+	const { attributes } = props;
+	const { textColor, customTextColor } = props.attributes;
+
+	let custom_color_class;
+	let custom_color_style = {};
+	if (textColor != undefined) {
+		custom_color_class = getColorClassName('color', textColor);
+	}
+	if (customTextColor != undefined) {
+		custom_color_style.color = customTextColor;
+	}
+	return (
+		<div className={className}>
+			<div
+				className="media">
+				<video loop="true" autoplay="autoplay" muted>
+					<source src={attributes.image} type="video/mp4" />
+				</video>
+			</div>
+			<div className="copy">
+				<div class="col-full">
+					<RichText.Content
+						className={"copy-hd " + custom_color_class} 
+						tagName="h2"
+						value={attributes.heading}
+					/>
+					<RichText.Content
+						className={"copy-bd " + custom_color_class} 
+						tagName="h3"
+						value={attributes.bodyContent}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+registerBlockType('borncreative/header-block-video', {
+	title: 'Video Header Block',
+	icon: 'format-video',
+	category: 'born-creative-blocks',
+
+	attributes: {
+		bodyContent: {
+			source: 'html',
+			selector: '.copy-bd'
+		},
+		heading: {
+			source: 'html',
+			selector: 'h2',
+		},
+		image: {
+			type: 'string',
+		},
+		textColor: {
+			type: 'string'
+		},
+		customTextColor: {
+			type: 'string'
+		},
+	},
+
+	edit: withColors({ textColor: 'color' })(edit_header_block),
+	save: save_header_block
+	
+});
+
+
