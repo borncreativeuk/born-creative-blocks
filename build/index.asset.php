f4176c5a466ee9f6227d27a1c5a6f7eb79df13a0
Born Creative <hi@born-creative.co.uk>
2023-04-19 14:43:12 +0100

    tidy ups

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'a0572b9949c788503a92');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'ce6a2b5cf8a2c3fd0192');
