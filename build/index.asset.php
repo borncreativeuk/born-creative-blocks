dc4c11e6661067a558b839cdf46c5a6477c881c2
Born Creative <hi@born-creative.co.uk>
2023-04-19 12:22:59 +0100

    the-solution-block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '8bf2d6122654f02fd9bc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'a6970a51e4b37f104005');
