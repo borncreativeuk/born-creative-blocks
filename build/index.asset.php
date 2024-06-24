94366920da2fd6ff3ca8446af22b4ac65b404d14
Born Creative <hi@born-creative.co.uk>
2024-06-24 15:55:25 +0100

    update deps -- untested

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'd5749029b574a3a1b57e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '30b49266afe1dad51f86');
